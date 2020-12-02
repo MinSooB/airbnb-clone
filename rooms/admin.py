6001d15f8fe9bfd8afc67e0ed70be02a31ad25ad
MinSooB <bae3003@naver.com>
2020-12-02 20:20:13 +0900

    1202
    MSB version

--- rooms/admin.py
+++ rooms/admin.py
@@ -34,7 +34,17 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "address",
+                    "price",
+                    "room_type",
+                )
+            },
         ),
         (
             "Times",
@@ -101,6 +111,8 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_photos(self, obj):
         return obj.photos.count()
+    
+    count_photos.short_description = "photo count"
 
     count_amenities.short_description = "hello sexy!"
 
