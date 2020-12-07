54f7c8d053a2a259b98b120fc95d4bc41021c789
MinSooB <bae3003@naver.com>
2020-12-07 10:23:08 +0900

    201207
    종휘야 도와줘 ㅠㅠㅠ

--- users/admin.py
+++ users/admin.py
@@ -27,6 +27,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -43,6 +44,9 @@ class CustomUserAdmin(UserAdmin):
         "superhost",
         "is_staff",
         "is_superuser",
+        "email_verified",
+        "email_secret",
+        "login_method",
     )
 
     list_filter = UserAdmin.list_filter + ("superhost",)
